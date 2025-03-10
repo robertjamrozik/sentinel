4ff41495a8a050e722dedb4e51115083e27ec94e
Ben Roesch <ben@cultivatelabs.com>
2025-03-09 20:22:58 -0400

    Fix specs

--- spec/models/raif/model_tool_invocation_spec.rb
+++ spec/models/raif/model_tool_invocation_spec.rb
@@ -6,22 +6,22 @@ require "support/test_model_tool"
 RSpec.describe Raif::ModelToolInvocation, type: :model do
   it "requires the tool_arguments schema to be valid based on the tool invoked" do
     completion = FB.build(:sentinel_completion)
-    tool_invocation = described_class.new(tool_arguments: nil, sentinel_completion: completion, tool_type: "Raif::TestModelTool")
+    tool_invocation = described_class.new(tool_arguments: nil, source: completion, tool_type: "Raif::TestModelTool")
     expect(tool_invocation.valid?).to eq(false)
     expect(tool_invocation.errors[:tool_arguments]).to include("does not match schema")
 
-    tool_invocation = described_class.new(tool_arguments: { "foo" => "bar" }, sentinel_completion: completion, tool_type: "Raif::TestModelTool")
+    tool_invocation = described_class.new(tool_arguments: { "foo" => "bar" }, source: completion, tool_type: "Raif::TestModelTool")
     expect(tool_invocation.valid?).to eq(false)
     expect(tool_invocation.errors[:tool_arguments]).to include("does not match schema")
 
-    tool_invocation = described_class.new(tool_arguments: [{ "foo" => "bar" }], sentinel_completion: completion, tool_type: "Raif::TestModelTool")
+    tool_invocation = described_class.new(tool_arguments: [{ "foo" => "bar" }], source: completion, tool_type: "Raif::TestModelTool")
     expect(tool_invocation.valid?).to eq(false)
     expect(tool_invocation.errors[:tool_arguments]).to include("does not match schema")
 
     # With valid schema
     tool_invocation = described_class.new(
       tool_arguments: [{ "title" => "foo", "description" => "bar" }],
-      sentinel_completion: completion,
+      source: completion,
       tool_type: "Raif::TestModelTool"
     )
     expect(tool_invocation.valid?).to eq(true)
