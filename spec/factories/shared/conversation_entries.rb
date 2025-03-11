e5d5f2105b6cd0e0d1e0cbccd0c59a7d82c029d2
Ben Roesch <ben@cultivatelabs.com>
2025-03-11 12:42:55 -0400

    More test coverage

--- spec/factories/shared/conversation_entries.rb
+++ spec/factories/shared/conversation_entries.rb
@@ -3,11 +3,11 @@
 FactoryBot.define do
   factory :sentinel_conversation_entry, class: "Raif::ConversationEntry" do
     sequence(:user_message){|i| "User message #{i} #{SecureRandom.hex(4)}" }
-    started_at { Time.current }
     creator { sentinel_conversation.creator }
 
     trait :completed do
       sequence(:model_response_message){|i| "Model response #{i} #{SecureRandom.hex(4)}" }
+      started_at { Time.current }
       completed_at { Time.current }
     end
   end
