c1445a736476d7debaa43983f01143e6140a3974
Ben Roesch <ben@cultivatelabs.com>
2025-04-21 14:09:07 -0400

    Add ApplicationConversation & ApplicationAgent,  similar to ApplicationTask and make generated conversation/agent types inherit from them

--- lib/generators/raif/agent/agent_generator.rb
+++ lib/generators/raif/agent/agent_generator.rb
@@ -1,22 +1,32 @@
 # frozen_string_literal: true
 
 module Raif
-  class AgentGenerator < Rails::Generators::NamedBase
-    source_root File.expand_path("templates", __dir__)
-    desc "Creates a new Raif::Agent subclass in app/models/sentinel/agents"
+  module Generators
+    class AgentGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path("templates", __dir__)
+      desc "Creates a new Raif::Agent subclass in app/models/sentinel/agents"
 
-    def create_agent
-      template "agent.rb.tt", "app/models/sentinel/agents/#{file_name}.rb"
-    end
+      def create_application_agent
+        template "application_agent.rb.tt", "app/models/sentinel/application_agent.rb" unless File.exist?("app/models/sentinel/application_agent.rb")
+      end
 
-  private
+      def create_agent
+        template "agent.rb.tt", "app/models/sentinel/agents/#{file_name}.rb"
+      end
 
-    def class_name
-      name.classify
-    end
+      def create_directory
+        empty_directory "app/models/sentinel/agents" unless File.directory?("app/models/sentinel/agents")
+      end
 
-    def file_name
-      name.underscore
+    private
+
+      def class_name
+        name.classify
+      end
+
+      def file_name
+        name.underscore
+      end
     end
   end
 end
